f3d263637d8dce59e8d3e67738a658821d2073ad
kowsi15 <84507663+kowsi15@users.noreply.github.com>
2021-05-28 16:04:18 +0530

    Update new3.java

--- new3.java
+++ new3.java
@@ -9,10 +9,8 @@ public class new3
 {
     public static void main( String[] args )
     {
-		Scanner sc= new Scanner(System.in); //System.in is a standard input stream  
-System.out.print("Enter a string: ");  
-String str= sc.nextLine();              //reads string   
-System.out.print("You have entered: "+str);             
+		            //reads string   
+System.out.print("You have entered: ");             
 }  
 
   
